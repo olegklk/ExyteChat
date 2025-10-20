bba7b09c489a09766c9951f2397553501fe2c505
Oleg Kolokolov <olegklk@gmail.com>
2025-10-20 17:20:22 +0300

    feat: add messageBuilder to ChatView and implement messageCell rendering

--- ChatExample/ChatExample/ConversationView.swift
+++ ChatExample/ChatExample/ConversationView.swift
@@ -55,13 +55,15 @@ struct ConversationView: View {
             .padding([.horizontal, .top])
             ChatView( messages: viewModel.messages,
                       chatType: .conversation,
-                      replyMode: .quote,
-                      didSendMessage: { draft in
-                Task { await viewModel.handleSend(draft) }
-            },
-                      reactionDelegate: nil,
-                      messageMenuAction: {
-                (action: DefaultMessageMenuAction, defaultActionClosure, message) in 
+                      replyMode: .quote) { draft in
+                            Task { await viewModel.handleSend(draft) }
+                        }
+                      messageBuilder: {
+                message, positionInGroup, positionInMessagesSection, positionInCommentsGroup,
+                showContextMenuClosure, messageActionClosure, showAttachmentClosure in
+                messageCell(message, positionInGroup, positionInCommentsGroup, showMenuClosure: showContextMenuClosure, actionClosure: messageActionClosure, attachmentClosure: showAttachmentClosure)
+            }
+              messageMenuAction: { (action: DefaultMessageMenuAction, defaultActionClosure, message) in
                 
                 switch action {
                     case .reply:
@@ -72,7 +74,8 @@ struct ConversationView: View {
                     })
                     case .copy: defaultActionClosure(message, .copy)
                 }
-            } )
+            }
+                      
             .swipeActions(edge: .leading, performsFirstActionWithFullSwipe: true,
                           items: [
                             SwipeAction(action: onReply, activeFor: { _ in true/*!$0.user.isCurrentUser*/}, background: .blue) {
@@ -134,6 +137,79 @@ struct ConversationView: View {
                 }
             }
         }
+    }
+    
+    @ViewBuilder
+    func messageCell(_ message: Message, _ positionInGroup: PositionInUserGroup, _ commentsPosition: CommentsPosition?, showMenuClosure: @escaping ()->(), actionClosure: @escaping (Message, DefaultMessageMenuAction) -> Void, attachmentClosure: @escaping (Attachment) -> Void) -> some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                if positionInGroup == .last {
+                    CachedAsyncImage(
+                        url: message.user.avatarURL,
+                        cacheKey: message.user.avatarCacheKey
+                    ) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } placeholder: {
+                        Rectangle().fill(Color.gray)
+                    }
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                } else {
+                    //вставь сюда блок который будет просто занимать такое же место 40x40px как и аватар, но будет не видим, в случае когда я не хочу здесь видеть аватар, но не хочу ломать структуру AI!
+                }
+
+                VStack(alignment: .leading, spacing: 6) {
+
+                    if !message.text.isEmpty {
+                        HStack {
+                            Text(message.text)
+                                .font(.system(size: 14)).fontWeight(.medium)
+                                .foregroundStyle(.black)
+                            Spacer()
+                        }
+                    }
+
+                    if !message.attachments.isEmpty {
+                        LazyVGrid(columns: Array(repeating: GridItem(), count: 2), spacing: 8) {
+                            ForEach(message.attachments) { attachment in
+                                AttachmentCell(attachment: attachment, size: CGSize(width: 150, height: 150)) { _ in
+                                    attachmentClosure(attachment)
+                                }
+                                .cornerRadius(12)
+                                .clipped()
+                            }
+                        }
+                        .frame(width: 308)
+                    }
+                    if positionInGroup == .last {
+                        HStack {
+                            Text(message.user.name)
+                                .font(.system(size: 12)).fontWeight(.semibold)
+                                .foregroundStyle(.gray)
+                            Spacer()
+                            Text(message.createdAt.formatAgo())
+                                .font(.system(size: 12)).fontWeight(.medium)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+            }
+            .padding(.leading, message.replyMessage != nil ? 40 : 0)
+//https://chat.gramatune.com/#conversation=c:d5c78030-fd50-11e4-8f87-e365c4562176:f5a53f40-d69b-11e4-b69a-e365c4562176&batch=bcdc8d52-66e6-46a9-8cf1-5a97d880be3d
+            if let commentsPosition {
+                if commentsPosition.isLastInCommentsGroup {
+                    Color.gray.frame(height: 0.5)
+                        .padding(.vertical, 10)
+                } else if commentsPosition.isLastInChat {
+                    Color.clear.frame(height: 5)
+                } else {
+                    Color.clear.frame(height: 10)
+                }
+            }
+        }
+        .padding(.horizontal, 18)
     }
     
     // Swipe Action
