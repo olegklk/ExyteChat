2b606cc26a298af8854c508f0879d6290622d484
Oleg Kolokolov <olegklk@gmail.com>
2025-10-24 13:53:46 +0300

    Tweaked theme and set it for entire app

--- ChatExample/ChatExample/ChatExampleApp.swift
+++ ChatExample/ChatExample/ChatExampleApp.swift
@@ -3,12 +3,24 @@
 //
 
 import SwiftUI
+import ExyteChat
 
 @main
 struct ChatExampleApp: App {
+    let veroTheme = ChatTheme(
+        colors: ChatTheme.Colors(
+                sendButtonBackground: .clear
+            ),
+            images: .init(
+                arrowSend:Image("action_chat_send", bundle: .current)
+        )
+    )
+    
     var body: some Scene {
         WindowGroup {
             UserSetupView()
+                .chatTheme(veroTheme)
+                .preferredColorScheme(.dark)
         }
     }
 }
